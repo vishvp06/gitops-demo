095032aede1d21257bdbfa8975fa0c6fd0c1b746
vishvp06 <vishvp06@gmail.com>
2026-04-17 11:16:37 +0530

    updated to vichu

--- app/backend/server.js
+++ app/backend/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 const frontendDir = path.join(__dirname, "..", "frontend");
 
 app.get("/", (_req, res) => {
-  res.send("Hello GitOps v1");
+  res.send("Hello GitOps vichu");
 });
 
 app.get("/healthz", (_req, res) => {
